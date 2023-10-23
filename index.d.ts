66e3d79ce64b3f36fc35cb74f94bf423a0ee9a9b
Vinícius C. Francisco <vini.francisco@more.tt>
2023-10-23 14:41:26 -0300

    fix: splash screen types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-declare module "react-native-lottie-splash-screen" {
-    export default class SplashScreen {
-        static hide(): void;
-        static show(): void;
-    }
+declare module "@henry-schein-brasil/react-native-lottie-splash-screen" {
+  export default class SplashScreen {
+    static hide(): void;
+    static show(): void;
+  }
 }
